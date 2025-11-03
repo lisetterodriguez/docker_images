17d4a48d607df69d87fb891ff851deadaf1224f1
Lisette Rodriguez <lisette.rodriguez@corp.glbb.ne.jp>
2025-11-03 09:55:52 +0900

    Add devcontainer files into dockerized_python folder

---
+++ dockerized_python/.devcontainer/set_python_env.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+PYTHON_ENV=$1
+
+python3 -m venv /opt/$PYTHON_ENV  \
+        && export PATH=/opt/$PYTHON_ENV/bin:$PATH \
+        && echo "source /opt/$PYTHON_ENV/bin/activate" >> ~/.bashrc
+
+source /opt/$PYTHON_ENV/bin/activate
+
+pip install --upgrade pip
+pip install -r ./requirements.txt
